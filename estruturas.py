ce85cd47dfb41548b3df4ede2cd6746bcdd50360
leandro-az <leandro_almeida@id.uff.br>
2017-09-16 21:50:18 -0300

    adicionando o desenho das retas,planos coloridos e rotações

--- estruturas.py
+++ estruturas.py
@@ -1,10 +1,74 @@
-vet = [[10, 40, 0,1],
-       [10, 100, 0,1],
-       [70, 170, 0,1],
-       [140, 170, 0,1],
-       [210, 100, 0,1],
-       [210, 40, 0,1],
-       [140, 0, 0,1],
-       [70, 0, 0,1]]
+import math
+
+matOri = [[10, 40, 0, 1], #0
+          [10, 100, 0,1], #1
+          [70, 170, 0,1], #2
+          [140, 170, 0,1],#3
+          [210, 100, 0,1],#4
+          [210, 40, 0,1],#5
+          [140, 0, 0,1],  #6
+          [70, 0, 0,1],  #7
+          [10, 40, 30, 1],#8
+          [10, 100, 30, 1],#9
+          [70, 170, 30, 1], #10
+          [140, 170, 30, 1],#11
+          [210, 100, 30, 1],#12
+          [210, 40, 30, 1],#13
+          [140, 0, 30, 1],#14
+          [70, 0, 30, 1]]#15
 x0=300
 y0=200
+
+pontosPoli=8
+
+matProPZ=[[1,0,0,0],
+          [0,1,0,0],
+          [0,0,0,0],
+          [0,0,0,1]]
+
+def multiplicaMatriz(A,B):
+    linhaA=len(A)
+    colunaA=len(A[0])
+    linhaB = len(B)
+    colunaB= len(B[0])
+    resp = []
+    if(colunaA != linhaB):
+        print("Impossivel Multiplicar Matrizes colunaA dif linhaB")
+        return resp
+    for l in range(linhaA):
+        resp.append([])
+        for c in range(colunaB):
+            resp[l].append(0)
+            for k in range(colunaA):
+                resp[l][c]+=A[l][k]*B[k][c]
+
+    return resp
+
+def fazRotacaoEmX(mat,angulo):
+
+    matRotX = [ [1, 0, 0, 0],
+                [0, math.cos(angulo), math.sin(angulo), 0],
+                [0, math.sin(angulo)*(-1),math.cos(angulo), 0],
+                [0, 0, 0, 1]]
+
+    return multiplicaMatriz(mat,matRotX)
+
+def fazRotacaoEmY(mat,angulo):
+
+    matRotY = [[math.cos(angulo),0,math.sin(angulo)*(-1),0],
+               [0,1,0,0],
+               [math.sin(angulo),0,math.cos(angulo),0],
+               [0,0,0,1]]
+
+    return multiplicaMatriz(mat, matRotY)
+
+def fazRotacaoEmZ(mat,angulo):
+
+    matRotZ = [[math.cos(angulo),math.sin(angulo),0,0],
+               [math.sin(angulo)*(-1),math.cos(angulo),0,0],
+               [0,0,1,0],
+               [0,0,0,1]]
+
+    return multiplicaMatriz(mat, matRotZ)
+
+
--- main.py
+++ main.py
@@ -1,29 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from graphics import *
 from estruturas import *
-import time
-
-def multiplicaMatriz(A,B):
-    linhaA=len(A)
-    colunaA=len(A[0])
-    linhaB = len(B)
-    colunaB= len(B[0])
-    resp = []
-    if(colunaA != linhaB):
-        print("Impossivel Multiplicar Matrizes colunaA dif linhaB")
-        return resp
-    for l in range(linhaA):
-        resp.append([])
-        for c in range(colunaB):
-            resp[l].append(0)
-            for k in range(colunaA):
-                resp[l][c]+=A[l][k]*B[k][c]
-
-    return resp
+import random, time
 
 
 def desenhaPontos(janela):
-    for i in range(len(vet)):
-        pt= Circle(Point(vet[i][0]+x0,vet[i][1]+y0),3)
+    for i in range(len(matOri)):
+        pt= Circle(Point(matOri[i][0] + x0, matOri[i][1] + y0), 3)
         pt.setFill("blue")
         pt.draw(janela)
 
@@ -51,30 +35,119 @@ def desenhaSetaD(janela):
 def desenhaPontoPassoAPasso():
     pass
 
-def desenhaRetasPassoAPasso(janela):
-    pontoAnt=Point(vet[0][0]+x0,vet[0][1]+y0)
+def desenhaRetasPassoAPasso(janela,matOpera):
+    pontoAnt=Point(matOpera[0][0] + x0, matOpera[0][1] + y0)
     pontoAtual=Point(0,0)
     pt = Circle(pontoAtual, 3)
-    for i in range(len(vet)):
+    for i in range(len(matOpera)):
+        # desenha o ponto no nosso caso circulo pequeno de raio 3
         pt.setFill("yellow")
-        pontoAtual=Point(vet[i][0]+x0,vet[i][1]+y0)
+        pontoAtual=Point(matOpera[i][0] + x0, matOpera[i][1] + y0)
         pt= Circle(pontoAtual,3)
         if(i>0):
            pt.setFill("red")
         pt.draw(janela)
-        time.sleep(1)
-        if(i!=0):
+        time.sleep(0.4)
+        #desenha reta somente se entre pontos do mesmo plano
+        if(i!=0 ):
             reta=Line(pontoAnt,pontoAtual)
             reta.draw(janela)
-            time.sleep(1)
+            time.sleep(0.4)
             pontoAnt=pontoAtual
-    pt.setFill("yellow")
-    pontoAnt = Point(vet[0][0] + x0, vet[0][1] + y0)
-    reta = Line(pontoAnt, pontoAtual)
-    reta.draw(janela)
+        #se ja chegou no ultimo vértice desenhado do plano, fecha o plano com a ultima reta
+        if (i > 0 and ((i + 1) % pontosPoli == 0)):
+            pt.setFill("yellow")
+            pontoAnt = Point(matOpera[i - (pontosPoli - 1)][0] + x0, matOpera[i - (pontosPoli - 1)][1] + y0)
+            reta = Line(pontoAnt, pontoAtual)
+            reta.draw(janela)
+            if(i+1<len(matOpera)):
+             pontoAnt = Point(matOpera[i+1][0] + x0, matOpera[i+1][1] + y0)
+    # desenha ultimas retas
+    for i in range(len(matOri)):
+        pontoAnt = Point(matOpera[i][0] + x0, matOpera[i][1] + y0)
 
-def desenhaPlanosPassoAPasso():
-    pass
+        for j in range(i+1,len(matOri)):
+            if(matOri[i][0]==matOri[j][0] and matOri[i][1]==matOri[j][1] and matOri[i][2]!=matOri[j][2]):
+
+                  pontoAtual= Point(matOpera[j][0] + x0, matOpera[j][1] + y0)
+                  reta = Line(pontoAnt, pontoAtual)
+                  reta.draw(janela)
+                  time.sleep(0.4)
+
+
+def desenhaPlanosPassoAPasso(janela,matOpera):
+
+
+    listaPLanos1=range(pontosPoli)
+    listaPLanos2 = range(pontosPoli)
+    p1=0
+    p2=0
+    for i in range(len(matOpera)):
+        if(i<pontosPoli):
+          listaPLanos1[p1]=Point(matOpera[i][0]+ x0,matOpera[i][1]  + y0)
+          p1=p1+1
+        else:
+          listaPLanos2[p2]=Point(matOpera[i][0]+ x0,matOpera[i][1]  + y0)
+          p2=p2+1
+
+#------------------------------- desenha o plano superior -----------------------------------------------------------------------------------
+
+    r = random.randrange(256)
+    b = random.randrange(256)
+    g = random.randrange(256)
+    cor = color_rgb(r, g, b)
+    poli1=Polygon(listaPLanos1)
+    poli1.setFill(cor)
+    poli1.draw(janela)
+    time.sleep(0.4)
+
+# ------------------------------- desenha os planos em volta ------------------------------------------------------------------------
+
+    for i in range(pontosPoli - 1):
+        r = random.randrange(256)
+        b = random.randrange(256)
+        g = random.randrange(256)
+        cor = color_rgb(r, g, b)
+        listaPLanos1 = range(4)
+        listaPLanos1[0] = Point(matOpera[i][0] + x0, matOpera[i][1] + y0)
+        listaPLanos1[1] = Point(matOpera[i + 1][0] + x0, matOpera[i + 1][1] + y0)
+        listaPLanos1[2] = Point(matOpera[i + 9][0] + x0, matOpera[i + 9][1] + y0)
+        listaPLanos1[3] = Point(matOpera[i + 8][0] + x0, matOpera[i + 8][1] + y0)
+        poli1 = Polygon(listaPLanos1)
+        poli1.setFill(cor)
+        poli1.draw(janela)
+        time.sleep(0.4)
+
+
+    r = random.randrange(256)
+    b = random.randrange(256)
+    g = random.randrange(256)
+    cor = color_rgb(r, g, b)
+    listaPLanos1 = range(4)
+    listaPLanos1[0] = Point(matOpera[0][0] + x0, matOpera[0][1] + y0)
+    listaPLanos1[1] =   Point(matOpera[7][0] + x0, matOpera[7][1] + y0)
+    listaPLanos1[2] =Point(matOpera[15][0] + x0, matOpera[15][1] + y0)
+    listaPLanos1[3] =  Point(matOpera[8][0] + x0, matOpera[8][1] + y0)
+    poli1 = Polygon(listaPLanos1)
+    poli1.setFill(cor)
+    poli1.draw(janela)
+    time.sleep(0.4)
+
+
+# ------------------------------- desenha o plano inferior -----------------------------------------------------------------------------
+
+    r = random.randrange(256)
+    b = random.randrange(256)
+    g = random.randrange(256)
+    cor = color_rgb(r, g, b)
+    poli2 = Polygon(listaPLanos2)
+    poli2.setFill(cor)
+    poli2.draw(janela)
+
+
+
+
+
 
 def colorePlanosPassoAPasso():
     pass
@@ -102,14 +175,19 @@ def main():
 
     resp=True
     while(resp):
-     desenhaRetasPassoAPasso(janela)
+     mat=fazRotacaoEmX(matOri,20)
+     mat = fazRotacaoEmY(mat, 90)
+
+     desenhaRetasPassoAPasso(janela,mat)
+
+     desenhaPlanosPassoAPasso(janela,mat)
+
      desenhaSetaE(janela)
      desenhaSetaD(janela)
      click = janela.getMouse().clone()
      if (click.getX() > 0):
          print(click.getX())
          resp= False
-
 
     janela.close()
 
